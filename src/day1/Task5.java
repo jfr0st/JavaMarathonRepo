2faf5f51decc3ff59f5fc7389d4bfe7d90bb36fb
Igor <jackfr0sst@yandex.ru>
2020-08-24 19:08:56 +0300

    Add Task1-6 day1

---
+++ src/day1/Task5.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        for(int age = 29;age < 100;age+=5){
+            System.out.println("Мой возраст " +  age);
+        }
+
+    }
+}
---
+++ src/day1/Task6.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int k = 7;
+        for(int i = 1;i<=9;i++){
+            System.out.println(k + " x " + i + " = " + k * i);
+        }
+    }
+}
