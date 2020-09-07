2d9a0600af9ea3906d943e7fad3e81ba97708fc4
Igor <jackfr0sst@yandex.ru>
2020-09-07 21:08:45 +0300

    add full task1 day15

--- src/day15/Task1.java
+++ src/day15/Task1.java
@@ -2,24 +2,24 @@ package day15;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) throws FileNotFoundException {
-        List<String> shoesArray = new ArrayList<>();
-
         File file = new File("shoes.csv");
+        File fileToWrite = new File("shoes0");
+        PrintWriter printWriter = new PrintWriter(fileToWrite);
+
         Scanner scanner = new Scanner(file);
         while (scanner.hasNext()){
-            String shoesStrimg = scanner.nextLine();
-            String[] string = shoesStrimg.split(";");
-            if(Integer.parseInt(string[2]) == 0) shoesArray.add(shoesStrimg);
+            String shoesString = scanner.nextLine();
+            String[] string = shoesString.split(";");
+            if(Integer.parseInt(string[2]) == 0) printWriter.println(shoesString);
         }
+        printWriter.close();
 
-        for(String str : shoesArray){
-            System.out.println(str);
-        }
     }
 }
